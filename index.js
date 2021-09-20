32cedf4d41ae9c132d9665ddb9081ab26a4b5d77
kritarth1107 <singhalkritarth@gmail.com>
2021-09-20 20:00:21 +0530

    LOMIT 50MB

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ require("dotenv").config();
 
 DB();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use("/manage",managesRouter);
 app.use("/schemes",schemesRouter);
 app.use("/folios",foliosRouter);
